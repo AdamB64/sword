c1ea64f6f48d5b2d4b9a21cdc8a4037d600216d2
Karma63 <adam2004buchan@outlook.com>
2025-04-03 17:49:35 +0100

    get the character counter working

--- ReadFileLineByLineUsingScanner.java
+++ ReadFileLineByLineUsingScanner.java
@@ -7,10 +7,25 @@ public class ReadFileLineByLineUsingScanner {
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(new File("sample.txt"));
+            String file = null;
 
             while (scanner.hasNextLine()) {
-                System.out.println(scanner.nextLine());
+                file = file + scanner.nextLine();
             }
+
+            int count = 0;
+
+            for (int i = 0; i < file.length(); i++) {
+                if (file.charAt(i) == ' ' || file.charAt(i) == '\n' || file.charAt(i) == '\t'
+                        || file.charAt(i) == '\r') {
+                } else {
+
+                    // Count the number of characters in the file
+                    count++;
+                }
+            }
+
+            System.out.println("Number of characters in the file: " + count);
 
             scanner.close();
         } catch (FileNotFoundException e) {
